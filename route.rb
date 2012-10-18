9094dd2155cf2a621706be097efa614dc6bbfd3b
Wes Williams <wes.williamz@gmail.com>
2012-10-18 12:20:19 -0700

    route string representation

--- route.rb
+++ route.rb
@@ -1,9 +1,52 @@
 class Route
-  attr_reader :distance
-
-  def initialize(origin, destination, distance)
+  
+  def initialize(origin = nil, destination = nil, distance = 0)
     @origin = origin
     @destination = destination
     @distance = distance
+    @connection = EndOfRoute.new
+  end
+
+  def connect(route)
+    @connection = route
+  end
+
+  def distance
+    @distance + @connection.distance
+  end
+
+  def destination_s
+    @connection.destination_s.empty? ? @destination : '' 
+  end
+
+  def connection_s
+    @origin + destination_s + @connection.connection_s
+  end
+
+  def to_s
+    connection_s + distance.to_s
+  end
+
+end
+
+class EndOfRoute
+  def distance
+    0
+  end
+
+  def origin_s
+    ''
+  end
+  
+  def destination_s
+    ''
+  end
+
+  def connection_s
+    ''
+  end
+
+  def to_s
+    ''
   end
 end
--- route_spec.rb
+++ route_spec.rb
@@ -2,10 +2,32 @@ require './route.rb'
 
 describe Route do
   
-  describe '#route_to' do
-    it 'should have a route for a connecting city' do
-      route = Route.new 'a', 'b', 5
+  context 'with no connections' do
+
+    let(:route) { Route.new 'a', 'b', 5 }
+
+    it 'returns only its distance' do
       route.distance.should eq(5)
+    end
+
+    it 'can be represented as a string' do
+      route.to_s.should eq('ab5')
+    end
+  end
+  
+  context 'with connections' do
+
+    let(:route_a) { Route.new 'a', 'b', 5 }
+    let(:route_b) { Route.new 'b', 'c', 4 }
+
+    it 'calculates combined distance of routes' do
+      route_a.connect route_b
+      route_a.distance.should eq(9) 
+    end
+
+    it 'can be represented as a string' do
+      route_a.connect route_b
+      route_a.to_s.should eq('abc9') 
     end
   end
 
